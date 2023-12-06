383f3b16308da0c259a8c6f8af32435c315ba83c
Fernando Acúrcio Sá Torres da Silva <fernandoacurcio@hotmail.com>
2023-12-06 18:32:33 +0000

    added generic repository

--- API/Program.cs
+++ API/Program.cs
@@ -37,6 +37,7 @@ internal class Program
         });
 
         builder.Services.AddScoped<IProductRepository, ProductRepository>();
+        builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
         var app = builder.Build();
 
---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        public Task<T> GetByIdAsync(int id);
+        public Task<IReadOnlyList<T>> ListAllAsync();
+    }
+}
